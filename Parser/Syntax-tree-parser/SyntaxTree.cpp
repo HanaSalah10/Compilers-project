d2781aa810a2dc37d71283d320508ff75a52150a
SalmaWaleeddd <salmawaleed2030@outlook.com>
2024-12-12 18:10:29 +0200

    added destructor again

--- Parser/Syntax-tree-parser/SyntaxTree.cpp
+++ Parser/Syntax-tree-parser/SyntaxTree.cpp
@@ -34,3 +34,12 @@ void SyntaxTree::setRoot(Node *root)
 }
 
 // Destructor to clean up resources (if needed, for deep copy implementation)
+
+SyntaxTree::~SyntaxTree()
+{
+    // If root node needs to be deleted, do it here
+    if (root != nullptr)
+    {
+        delete root; // This will recursively delete all children due to the Node destructor
+    }
+}
